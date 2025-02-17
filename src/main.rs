654c2dfafac4a188379a223eba399b4d8b4242c3
“oliver-illum” <oliverbregneberg@gmail.com>
2025-02-17 23:02:09 +0100

    project done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,74 @@
-use micrograd_rust::engine::*;
+use micrograd_rust::{engine::*, nn::*};
 
 fn main() {
+    let xs: Vec<Vec<ValueWrapper>> = vec![
+        vec![Value::new(2.0), Value::new(3.0), Value::new(-1.0)],
+        vec![Value::new(3.0), Value::new(-1.0), Value::new(0.5)],
+        vec![Value::new(0.5), Value::new(1.0), Value::new(1.0)],
+        vec![Value::new(1.0), Value::new(1.0), Value::new(-1.0)],
+    ];
+
+    let ys: Vec<ValueWrapper> = vec![1.0.into(), (-1.0).into(), (-1.0).into(), 1.0.into()];
+
+    let mlp = MultiLayerPerceptron::new(3, &[4, 1]);
+
+    println!("First predictions:");
+    for (i, x) in xs.iter().enumerate() {
+        let output = mlp.call(x.clone());
+        let prediction = output[0].0.borrow().data;
+        println!("Sample {}: Prediction = {}", i, prediction);
+    }
+    println!();
+    for epoch in 0..=100 {
+        let mut ypreds: Vec<ValueWrapper> = Vec::new();
+        for x in &xs {
+            let output = mlp.call(x.clone());
+            ypreds.push(output[0].clone());
+        }
+
+        let mut loss = Value::new(0.0);
+        for (ygt, ypred) in ys.iter().zip(ypreds.iter()) {
+            let error = ypred.clone() - ygt.clone();
+            let error_sq = error.pow(2.0);
+            loss = loss + error_sq;
+        }
+
+        for p in mlp.parameters() {
+            p.0.borrow_mut().grad = 0.0;
+        }
+
+        let loss_clone = loss.clone();
+        loss.backward();
+
+        for p in mlp.parameters() {
+            let mut param = p.0.borrow_mut();
+            param.data += -0.1 * param.grad;
+        }
+        if epoch % 10 == 0 {
+            println!("Epoch {}: Loss = {}", epoch, loss_clone.0.borrow().data);
+        }
+    }
+    println!();
+    println!("Final predictions:");
+    for (i, x) in xs.iter().enumerate() {
+        let output = mlp.call(x.clone());
+        let prediction = output[0].0.borrow().data;
+        println!("Sample {}: Prediction = {}", i, prediction);
+    }
+}
+
+#[allow(unused)]
+fn initialize_mpl() {
+    let mlp = MultiLayerPerceptron::new(3, &[4, 1]);
+    let x = vec![Value::new(1.0), Value::new(2.0), Value::new(3.0)];
+
+    let output = mlp.call(x);
+    println!("Output: {:?}", output);
+    println!("Prediction: {}", output[0].0.borrow().data);
+}
+
+#[allow(unused)]
+fn test_value() {
     let x1 = Value::new(2.0);
     let x2 = Value::new(0.0);
 
--- src/nn.rs
+++ src/nn.rs
@@ -1,5 +1,83 @@
-pub struct Neuron {}
+use crate::engine::{Value, ValueWrapper};
+use rand::Rng;
 
-pub struct Layer {}
+pub struct Neuron {
+    pub w: Vec<ValueWrapper>,
+    pub b: ValueWrapper,
+}
 
-pub struct MultiLayerPerceptron {}
+impl Neuron {
+    pub fn new(nin: usize) -> Self {
+        let mut rng = rand::rng();
+        Neuron {
+            w: (0..nin)
+                .map(|_| Value::new(rng.random_range(-1.0..1.0)))
+                .collect(),
+            b: Value::new(rng.random_range(-1.0..1.0)),
+        }
+    }
+
+    pub fn call(&self, x: &[ValueWrapper]) -> ValueWrapper {
+        let mut act = self.b.clone();
+        for (wi, xi) in self.w.iter().zip(x.iter()) {
+            act = act + (wi.clone() * xi.clone());
+        }
+        act.tanh()
+    }
+
+    pub fn parameters(&self) -> Vec<ValueWrapper> {
+        let mut params = self.w.clone();
+        params.push(self.b.clone());
+        params
+    }
+}
+
+pub struct Layer {
+    pub neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    pub fn new(nin: usize, nout: usize) -> Self {
+        let neurons = (0..nout).map(|_| Neuron::new(nin)).collect();
+        Layer { neurons }
+    }
+
+    pub fn call(&self, x: &[ValueWrapper]) -> Vec<ValueWrapper> {
+        self.neurons.iter().map(|n| n.call(x)).collect()
+    }
+
+    pub fn parameters(&self) -> Vec<ValueWrapper> {
+        self.neurons.iter().flat_map(|n| n.parameters()).collect()
+    }
+}
+
+pub struct MultiLayerPerceptron {
+    pub layers: Vec<Layer>,
+}
+
+impl MultiLayerPerceptron {
+    pub fn new(nin: usize, nouts: &[usize]) -> Self {
+        let mut layers = Vec::new();
+        let mut input_size = nin;
+        for &nout in nouts {
+            layers.push(Layer::new(input_size, nout));
+            input_size = nout;
+        }
+
+        MultiLayerPerceptron { layers }
+    }
+
+    pub fn call(&self, mut x: Vec<ValueWrapper>) -> Vec<ValueWrapper> {
+        for layer in &self.layers {
+            x = layer.call(&x)
+        }
+        x
+    }
+
+    pub fn parameters(&self) -> Vec<ValueWrapper> {
+        self.layers
+            .iter()
+            .flat_map(|layer| layer.parameters())
+            .collect()
+    }
+}
